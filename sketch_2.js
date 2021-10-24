b3ed0391a6c7dfeac90a5f1e2e8033afcd4ad785
lociobello <lorenzo2.bernini@mail.polimi.it>
2021-10-25 00:21:44 +0200

    tutto nel draw

--- sketch_2.js
+++ sketch_2.js
@@ -8,39 +8,21 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // background(255);
+}
+
+function draw() {
+  background(255);
 
   let presidenti = data.presidenti;
 
   for (let i = 0; i < presidenti.length; i++) {
     const presidente = presidenti[i];
     const name = presidente.name;
-    const offset = width / presidenti.length;
-    const partito = presidente.party;
-    const ngoverni = presidente.govNum;
-
-    push();
-    angleMode(DEGREES);
-    rotate(90);
-    translate(height / 2, -(offset * i + 7));
-    fill(0);
-    textSize(20);
-    textFont(monumentGrotesk);
-    text(name, 0, 0);
-    text(partito, height / 4, 0);
-    text("X " + ngoverni, height / 2.3, 0);
-    pop();
-  }
-}
-
-function draw() {
-  let presidenti = data.presidenti;
-
-  for (let i = 0; i < presidenti.length; i++) {
-    const presidente = presidenti[i];
     const size = presidente.daysInOffice;
     const offset = width / presidenti.length;
     const foto = presidente.image;
+    const partito = presidente.party;
+    const ngoverni = presidente.govNum;
 
     let img = createImg(foto);
     img.hide();
@@ -54,6 +36,18 @@ function draw() {
     rect(0, 0, size / 10, size / 7.5);
     //divisori
     rect(0, 0, width, height);
+    pop();
+
+    push();
+    angleMode(DEGREES);
+    rotate(90);
+    translate(height / 2, -(offset * i + 7));
+    fill(0);
+    textSize(20);
+    textFont(monumentGrotesk);
+    text(name, 0, 0);
+    text(partito, height / 4, 0);
+    text("X " + ngoverni, height / 2.3, 0);
     pop();
   }
 }
