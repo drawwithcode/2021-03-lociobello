c552f56a6335886f63a0155ee777ae0788151ff3
lociobello <lorenzo2.bernini@mail.polimi.it>
2021-10-24 23:52:30 +0200

    caricamento foto nel draw

--- sketch.js
+++ sketch.js
@@ -16,24 +16,9 @@ function setup() {
   for (let i = 0; i < presidenti.length; i++) {
     const presidente = presidenti[i];
     const name = presidente.name;
-    const size = presidente.daysInOffice;
     const offset = width / presidenti.length;
-    const foto = presidente.image;
     const partito = presidente.party;
     const ngoverni = presidente.govNum;
-    const dal = presidente.tookOffice;
-
-    let img = createImg(foto);
-    img.hide();
-
-    push();
-    translate(offset * i, 0);
-    image(img, 0, 0, size / 10, size / 7.5);
-    //cornice
-    noFill();
-    rect(0, 0, size / 10, size / 7.5);
-    rect(0, 0, width, height);
-    pop();
 
     push();
     angleMode(DEGREES);
@@ -42,8 +27,6 @@ function setup() {
     fill(0);
     textSize(20);
     textFont(monumentGrotesk);
-    // text(partito, 5, 55);
-    // text(dal, 5, 30);
     text(name, 0, 0);
     text(partito, height / 4, 0);
     text("X " + ngoverni, height / 2.3, 0);
@@ -52,4 +35,27 @@ function setup() {
   }
 }
 
-function draw() {}
+function draw() {
+  let presidenti = data.presidenti;
+
+  for (let i = 0; i < presidenti.length; i++) {
+    const presidente = presidenti[i];
+    const size = presidente.daysInOffice;
+    const offset = width / presidenti.length;
+    const foto = presidente.image;
+
+    let img = createImg(foto);
+    img.hide();
+
+    push();
+    translate(offset * i, 0);
+    //foto
+    image(img, 0, 0, size / 10, size / 7.5);
+    //cornice
+    noFill();
+    rect(0, 0, size / 10, size / 7.5);
+    //divisori
+    rect(0, 0, width, height);
+    pop();
+  }
+}
