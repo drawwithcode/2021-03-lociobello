4c14f526c5580c54d472c9e1c355500c9c18fd86
lociobello <lorenzo2.bernini@mail.polimi.it>
2021-10-26 00:06:49 +0200

    audio

--- sketch_2.js
+++ sketch_2.js
@@ -1,9 +1,23 @@
 let data;
 let monumentGrotesk;
 
+let renzi;
+let craxi;
+let berlusconi;
+let andreotti;
+
+renziFill = 255;
+craxiFill = 255;
+berlusconiFill = 255;
+andreottiFill = 255;
+
 function preload() {
   data = loadJSON("assets/italy_prime_ministers.json");
   monumentGrotesk = loadFont("assets/MonumentGrotesk-Regular.ttf");
+  renzi = loadSound("assets/renzi.mp3");
+  craxi = loadSound("assets/craxi.mp3");
+  berlusconi = loadSound("assets/berlusconi.mp3");
+  andreotti = loadSound("assets/andreotti.mp3");
 }
 
 function setup() {
@@ -36,7 +50,7 @@ function draw() {
     noFill();
     rect(0, 0, size / 10, size / 7.5);
     //divisori
-    rect(0, 0, width, height);
+    sezioni = rect(0, 0, width, height);
     pop();
 
     //testi
@@ -49,9 +63,71 @@ function draw() {
     textFont(monumentGrotesk);
     text(name, 0, 0);
     text(partito, height / 4, 0);
-    text("X " + ngoverni, height / 2.3, 0);
+    text("X " + ngoverni, height / 2.4, 0);
     pop();
   }
+
+  //audio renzi
+  if (mouseX > (width / 30) * 26 && mouseX < (width / 30) * 27) {
+    if (renzi.isPlaying() == false) {
+      renzi.play();
+      renziFill = 0;
+    }
+  } else {
+    renzi.stop();
+    renziFill = 255;
+  }
+
+  //audio craxi
+  if (mouseX > (width / 30) * 15 && mouseX < (width / 30) * 16) {
+    if (craxi.isPlaying() == false) {
+      craxi.play();
+      craxiFill = 0;
+    }
+  } else {
+    craxi.stop();
+    craxiFill = 255;
+  }
+
+  //audio berlusconi
+  if (mouseX > (width / 30) * 20 && mouseX < (width / 30) * 21) {
+    if (berlusconi.isPlaying() == false) {
+      berlusconi.play();
+      berlusconiFill = 0;
+    }
+  } else {
+    berlusconi.stop();
+    berlusconiFill = 255;
+  }
+
+  //audio andreotti
+  if (mouseX > (width / 30) * 11 && mouseX < (width / 30) * 12) {
+    if (andreotti.isPlaying() == false) {
+      andreotti.play();
+      andreottiFill = 0;
+    }
+  } else {
+    andreotti.stop();
+    andreottiFill = 255;
+  }
+
+  fill(renziFill);
+  circle((width / 30) * 26 + 12, height - 16, 12);
+  fill(craxiFill);
+  circle((width / 30) * 15 + 12, height - 16, 12);
+  fill(berlusconiFill);
+  circle((width / 30) * 20 + 12, height - 16, 12);
+  fill(andreottiFill);
+  circle((width / 30) * 11 + 12, height - 16, 12);
+
+  // if (mouseX > width / 30 && mouseX < (width / 30) * 2) {
+  //   push();
+  //   blendMode(MULTIPLY);
+  //   noStroke();
+  //   fill("red");
+  //   rect(width / 30, 0, width / 30, height);
+  //   pop();
+  // }
 }
 
 function windowResized() {
